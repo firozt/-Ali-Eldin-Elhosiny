f066cc7693d8b437ab6ed4317dcc36ba49b8e6ff
firozt <firozt03@gmail.com>
2025-11-01 21:57:11 +0000

    good update

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
+'use client'
 import Image from 'next/image'
 import './index.css'
 
 const Navbar = () => {
   return (
 		<div className="w-10/12 flex justify-between m-auto pt-10 mb-5">
-			<Image width={80} height={80} src={'/ali-logo.svg'} alt='Ali Eldin Elhosiny logo' />
+			<Image className='cursor-pointer' onClick={() => window.location.reload()} width={80} height={80} src={'/ali-logo.svg'} alt='Ali Eldin Elhosiny logo' />
 			<div id='nav-container' className="flex row m-5 gap-10">
 				<h1>Profile</h1>
 				<h1>Blog</h1>
